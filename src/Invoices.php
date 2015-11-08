31a08efec905bed785763e8411974ad0e3065965
Bruno Moreira De Barros <bruno@terraduo.com>
2015-11-08 19:58:41 +0000

    Use 'status' to return whether or not the email has sent successfully.

--- src/Invoices.php
+++ src/Invoices.php
@@ -9,7 +9,8 @@ class Invoices {
     }
 
     public static function send(Server $server, $unique_id) {
-        return $server->post("invoices/send", array("unique_id" => $unique_id));
+        $result = $server->post("invoices/send", array("unique_id" => $unique_id));
+        return $result['status'];
     }
 
 }
