efc012274c8e21323b4d953d74e838649ea9d322
Bruno Moreira De Barros <bruno@terraduo.com>
2016-07-23 16:58:13 +0100

    Sets different API keys and details depending on the environment.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 
 require dirname(__FILE__) . '/../vendor/autoload.php';
+
+if (gethostname() == "Nagash.local") {
+    define("PANCAKE_URL", "http://pancakepayments.dev");
+    define("PANCAKE_API_KEY", "y2x45e07cavdpiy18c95r35u40eo49xg4lyv7hjw");
+} else {
+    define("PANCAKE_URL", "https://demo.pancakeapp.com");
+    define("PANCAKE_API_KEY", "y2x45e07cavdpiy18c95r35u40eo49xg4lyv7hjw");
+}
