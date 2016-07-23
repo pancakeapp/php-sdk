60153ca08003aba7dfaa65d002efb352f45bcb2e
Bruno Moreira De Barros <bruno@terraduo.com>
2016-07-23 16:56:43 +0100

    Adds DomainException and RequestException for more expressive exceptions.

---
+++ src/DomainException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pancake;
+
+use Exception;
+
+/**
+ * Domain Exceptions.<br />
+ * Thrown by the SDK when attempting to write to a property that is read-only.
+ *
+ * @package  Pancake
+ * @author   Pancake Dev Team <support@pancakeapp.com>
+ * @license  https://www.pancakeapp.com/license Pancake End User License Agreement
+ * @link     https://www.pancakeapp.com
+ */
+class DomainException extends Exception
+{
+
+}
---
+++ src/RequestException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pancake;
+
+use Exception;
+
+/**
+ * Request Exceptions.<br />
+ * Thrown by the SDK when the request is incorrect and the API returns a status of false.
+ *
+ * @package  Pancake
+ * @author   Pancake Dev Team <support@pancakeapp.com>
+ * @license  https://www.pancakeapp.com/license Pancake End User License Agreement
+ * @link     https://www.pancakeapp.com
+ */
+class RequestException extends Exception
+{
+
+}
