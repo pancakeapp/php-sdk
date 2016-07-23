4fe247cdbe01f138e897e33938a1fa39d9bbe26a
Bruno Moreira De Barros <bruno@terraduo.com>
2016-07-23 16:57:34 +0100

    Adds RuntimeException for more expressive exceptions.

---
+++ src/RuntimeException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pancake;
+
+use Exception;
+
+/**
+ * Runtime Exceptions.<br />
+ * Thrown by the SDK when an error happens on runtime that can't be categorised.
+ *
+ * @package  Pancake
+ * @author   Pancake Dev Team <support@pancakeapp.com>
+ * @license  https://www.pancakeapp.com/license Pancake End User License Agreement
+ * @link     https://www.pancakeapp.com
+ */
+class RuntimeException extends Exception
+{
+
+}
